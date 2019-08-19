0badd9c5f6b791c02190433947c3961d517cefa3
capitanov <sallador@bk.ru>
2019-08-19 12:36:39 +0300

    Improve main script

--- src/hh_research.py
+++ src/hh_research.py
@@ -46,6 +46,9 @@ OR CORRECTION.
 
 ------------------------------------------------------------------------
 """
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urlencode
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -56,16 +59,11 @@ import requests
 import argparse
 import hashlib
 import pickle
+import nltk
 import re
 import os
 
-from concurrent.futures import ThreadPoolExecutor
-from urllib.parse import urlencode
-from tqdm import tqdm
-
-import nltk
 # nltk.download('stopwords')
-from nltk.corpus import stopwords
 
 CACHE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'cache')
 API_BASE_URL = 'https://api.hh.ru/vacancies'
@@ -206,6 +204,15 @@ def get_vacancies(query, refresh=False):
 
 
 def prepare_df(dct_df):
+    """
+    Prepare data frame and save results
+
+    Parameters
+    ----------
+    dct_df: list
+        List of parsed json dicts
+
+    """
     # List of columns
     df_cols = ['Id',
                'Employer',
@@ -222,13 +229,101 @@ def prepare_df(dct_df):
     df = pd.DataFrame(data=dct_df, columns=df_cols)
     # Print some info from data frame
     print(
-        df[df['Salary']][['Employer', 'From', 'To', 'Experience']][0:10]
+        df[df['Salary']][['Employer', 'From', 'To', 'Experience', 'Schedule']][0:10]
     )
     # Save to file
     df.to_csv(r'hh_data.csv', index=False)
 
 
+def analyze_df():
+    """
+    Load data frame and analyze results
+
+    """
+
+    sns.set()
+    print('\n\nLoad table and analyze results:')
+    df = pd.read_csv('hh_data.csv')
+    print(df[df['Salary']][0:7])
+
+    print('\nNumber of vacancies: {}'.format(df['Id'].count()))
+    print('\nVacancy with max salary: ')
+    print(df.iloc[df[['From', 'To']].idxmax()])
+    print('\nVacancy with min salary: ')
+    print(df.iloc[df[['From', 'To']].idxmin()])
+
+    print('\nDescribe salary data frame: ')
+    df_stat = df[['From', 'To']].describe().applymap(np.int32)
+    print(df_stat.iloc[list(range(4)) + [-1]])
+
+    print('\nAverage statistics (average filter for "From"-"To" parameters):')
+    comb_from_to = np.nanmean(df[df['Salary']][['From', 'To']].to_numpy(), axis=1)
+    print('Describe salary series:')
+    print('Min    : %d' % np.min(comb_from_to))
+    print('Max    : %d' % np.max(comb_from_to))
+    print('Mean   : %d' % np.mean(comb_from_to))
+    print('Median : %d' % np.median(comb_from_to))
+
+    print('\nMost frequently used keys in the all vacancies:')
+    # Collect keys from df
+    keys_df = df['Keys'].to_list()
+    # Create a list of keys for all vacancies
+    lst_keys = []
+    for keys_elem in keys_df:
+        for el in keys_elem[1:-1].split(', '):
+            if el != '':
+                lst_keys.append(re.sub('\'', '', el.lower()))
+    # Unique keys and their counter
+    set_keys = set(lst_keys)
+    # Dict: {Key: Count}
+    dct_keys = {el: lst_keys.count(el) for el in set_keys}
+    # Sorted dict
+    srt_keys = dict(sorted(dct_keys.items(), key=lambda x: x[1], reverse=True))
+    # Return pandas series
+    most_keys = pd.Series(srt_keys, name='Keys')
+    print(most_keys[:7])
+
+    print('\nMost frequently used words in the all vacancies:')
+    # Collect keys from df
+    words_df = df['Description'].to_list()
+    # Long string - combine descriptions
+    words_ls = ' '.join(
+        [re.sub(' +', ' ', re.sub('\d+', '', el.strip().lower())) for el in words_df]
+    )
+    # Find all words
+    words_re = re.findall('[a-zA-Z]+', words_ls)
+    # Filter words with length < 3
+    words_l2 = [el for el in words_re if len(el) > 2]
+    # Unique words
+    words_st = set(words_l2)
+    # Remove 'stop words'
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    # XOR for dictionary
+    words_st ^= stop_words
+    words_st ^= {'amp', 'quot'}
+    # Dictionary - {Word: Counter}
+    words_cnt = {el : words_l2.count(el) for el in words_st}
+    # Pandas series
+    most_words = pd.Series(dict(sorted(words_cnt.items(), key=lambda x: x[1], reverse=True)))
+    print(most_words[:7])
+
+    print('\nPlot results. Close figure box to continue...')
+    fz = plt.figure(figsize=(12, 4), dpi=80)
+    fz.add_subplot(1, 2, 1)
+    plt.title('From / To Box-plot')
+    sns.boxplot(data=df[['From', 'To']].dropna(), width=0.4)
+    fz.add_subplot(1, 2, 2)
+    plt.title('From / To Swarm-plot')
+    sns.swarmplot(data=df[['From', 'To']].dropna(), size=6)
+    plt.tight_layout()
+    plt.show()
+
+
 def run():
+    """
+    Main function - combine all methods together
+
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('query', help='Search query (e.g. "Machine learning")')
     parser.add_argument(
@@ -238,14 +333,13 @@ def run():
     args = parser.parse_args()
 
     update_exchange_rates()
-
     print('Collect data from JSON. Create list of vacancies...')
     vac_list = get_vacancies(args.query, args.refresh)
     print('Prepare data frame...')
     prepare_df(vac_list)
-    print('Done. Exit()')
+    analyze_df()
+    print('Done! Exit()')
 
 
 if __name__ == "__main__":
     run()
-
