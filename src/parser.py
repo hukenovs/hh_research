b821922e6b32b3f6861510798cda1908f5ed2478
Alexander Kapitanov <sallador@bk.ru>
2020-10-31 23:07:25 +0300

    Add parser

---
+++ src/parser.py
@@ -0,0 +1,166 @@
+r"""Parse command line arguments
+
+Command parameters:
+    refresh         : bool  - Refresh data from remote server.
+    max_workers     : int   - Number of workers for threading.
+    options         : dict  - Options for GET request to hh api.
+
+Example:
+    options:
+        {
+            "text": "Python Developer",
+            "area": 1,
+            "per_page": 50
+        }
+
+Parser parameters:
+    update           : bool  - Update JSON config if needed.
+
+------------------------------------------------------------------------
+
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
+Copyright (c) 2019 Kapitanov Alexander
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND
+PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE
+DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR
+OR CORRECTION.
+
+------------------------------------------------------------------------
+"""
+
+# Authors       : Alexander Kapitanov
+# ...
+# Contacts      : <empty>
+# License       : GNU GENERAL PUBLIC LICENSE
+
+import json
+import argparse
+from typing import Optional, Sequence
+
+
+class Settings:
+    r"""Researcher parameters
+
+
+    Parameters
+    ----------
+    config_path: str
+        Path to config file
+
+    input_args: tuple
+        Command line arguments for tests.
+
+    Attributes
+    ----------
+
+    refresh : bool
+        Refresh data from remote server.
+
+    max_workers : int
+        Number of workers for threading.
+
+    options : dict
+        Options for GET request to API.
+
+    """
+
+    def __init__(self, config_path: str, input_args: Optional[Sequence[str]] = None):
+        self.refresh: bool = False
+        self.max_workers: int = 1
+        self.options: Optional[dict] = None
+
+        # Get config from file
+        with open(config_path, "r") as cfg:
+            config: dict = json.load(cfg)
+
+        # Update config from command line
+        params = self.__update_params(input_args=input_args)
+
+        for key, value in params.items():
+            if value is not None:
+                if key in config:
+                    config[key] = value
+                if "options" in config and key in config["options"]:
+                    config["options"][key] = value
+
+        if params["update"]:
+            with open(config_path, "w") as cfg:
+                json.dump(config, cfg, indent=2)
+
+        # Update attributes:
+        for key, value in config.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
+    def __repr__(self):
+        txt = "\n".join([f"{k :<16}: {v}" for k, v in self.__dict__.items()])
+        return f"Parameters:\n{txt}"
+
+    @staticmethod
+    def __update_params(input_args: Optional[Sequence[str]] = None) -> dict:
+        """Read arguments from command line.
+
+        Returns
+        -------
+        arguments : dict
+            Parsed arguments from command line. Note: some arguments are positional.
+
+        """
+
+        parser = argparse.ArgumentParser(description="HeadHunter vacancies researcher")
+        parser.add_argument(
+            "--text",
+            action="store",
+            type=str,
+            default=None,
+            help='Search query text (e.g. "Machine learning")'
+        )
+        parser.add_argument(
+            "--max_workers",
+            action="store",
+            type=int,
+            default=None,
+            help='Number of workers for multithreading.'
+        )
+        parser.add_argument(
+            "--refresh",
+            help="Refresh cached data from HH API",
+            action="store_true",
+            default=None,
+        )
+
+        parser.add_argument(
+            "--update", action="store_true", help="Save command line args to file in JSON format."
+        )
+
+        args, unknown = parser.parse_known_args(input_args)
+        return vars(args)
+
+
+if __name__ == "__main__":
+    settings = Settings(
+        config_path="../settings.json",
+        input_args=(
+            "--max_workers", "5",
+            "--refresh",
+            "--text", "Data Scientist"
+        )
+    )
+
+    print(settings)
